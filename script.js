389b2321297304ae6bffd7718d0ddc0a12868bcf
Raffy Nugraha <raffy.nugraha12@outlook.com>
2025-03-24 19:47:08 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,143 @@
+const card1 = document.getElementById("card1");
+const img1 = card1.querySelector("img");
+const text1 = card1.querySelector("h3");
+const originalSrc1 = img1.src;
+
+card1.addEventListener("mouseenter", function () {
+    img1.src = "https://assets.promediateknologi.id/crop/0x0:0x0/0x0/webp/photo/indizone/2020/03/30/1xsOPA/t_5e81f00c4785f.jpg";
+    text1.style.color = "green";
+    text1.style.transition = "color 0.2s ease-in-out";
+});
+
+card1.addEventListener("mouseleave", function () {
+    img1.src = originalSrc1;
+    text1.style.color = "black";
+});
+
+const card2 = document.getElementById("card2");
+const img2 = card2.querySelector("img");
+const text2 = card2.querySelector("h3");
+const originalSrc2 = img2.src;
+
+card2.addEventListener("mouseenter", function () {
+    img2.src = "https://www.sajianlezat.com/wp-content/Resep-Tumis-Genjer-Tauco.webp";
+    text2.style.color = "green";
+    text2.style.transition = "color 0.2s ease-in-out";
+});
+
+card2.addEventListener("mouseleave", function () {
+    img2.src = originalSrc2;
+    text2.style.color = "black";
+});
+
+const card3 = document.getElementById("card3");
+const img3 = card3.querySelector("img");
+const text3 = card3.querySelector("h3");
+const originalSrc3 = img3.src;
+
+card3.addEventListener("mouseenter", function () {
+    img3.src = "https://asset.kompas.com/crops/H_XI84PJ3ncIRUgTavITj6tPdpA=/1x0:1280x853/1200x800/data/photo/2022/10/04/633b5a997d22d.jpg";
+    text3.style.color = "green";
+    text3.style.transition = "color 0.2s ease-in-out";
+});
+
+card3.addEventListener("mouseleave", function () {
+    img3.src = originalSrc3;
+    text3.style.color = "black";
+});
+
+const card4 = document.getElementById("card4");
+const img4 = card4.querySelector("img");
+const text4 = card4.querySelector("h3");
+const originalSrc4 = img4.src;
+
+card4.addEventListener("mouseenter", function () {
+    img4.src = "https://cdn0-production-images-kly.akamaized.net/1z_vU8fpgs2wKRVjAxoNMkCd6mY=/680x383/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/2487625/original/025964600_1543334493-resep-cara-membuat-tempe-goreng-tepung-daun-bawang-gurih-kebangetan.jpg";
+    text4.style.color = "green";
+    text4.style.transition = "color 0.2s ease-in-out";
+});
+
+card4.addEventListener("mouseleave", function () {
+    img4.src = originalSrc4;
+    text4.style.color = "black";
+});
+
+const card5 = document.getElementById("card5");
+const img5 = card5.querySelector("img");
+const text5 = card5.querySelector("h3");
+const originalSrc5 = img5.src;
+
+card5.addEventListener("mouseenter", function () {
+    img5.src = "https://www.dapurkobe.co.id/wp-content/uploads/kikil-pedas-bumbu-serai.jpg";
+    text5.style.color = "green";
+    text5.style.transition = "color 0.2s ease-in-out";
+});
+
+card5.addEventListener("mouseleave", function () {
+    img5.src = originalSrc5;
+    text5.style.color = "black";
+});
+
+const card6 = document.getElementById("card6");
+const img6 = card6.querySelector("img");
+const text6 = card6.querySelector("h3");
+const originalSrc6 = img6.src;
+
+card6.addEventListener("mouseenter", function () {
+    img6.src = "https://cdn.grid.id/crop/0x0:0x0/700x465/photo/2019/11/30/2053585552.jpg";
+    text6.style.color = "green";
+    text6.style.transition = "color 0.2s ease-in-out";
+});
+
+card6.addEventListener("mouseleave", function () {
+    img6.src = originalSrc6;
+    text6.style.color = "black";
+});
+
+const card7 = document.getElementById("card7");
+const img7 = card7.querySelector("img");
+const text7 = card7.querySelector("h3");
+const originalSrc7 = img7.src;
+
+card7.addEventListener("mouseenter", function () {
+    img7.src = "https://akcdn.detik.net.id/community/media/visual/2024/03/05/resep-nasi-goreng-kunyit-dan-ayam_43.jpeg?w=700&q=90";
+    text7.style.color = "green";
+    text7.style.transition = "color 0.2s ease-in-out";
+});
+
+card7.addEventListener("mouseleave", function () {
+    img7.src = originalSrc7;
+    text7.style.color = "black";
+});
+
+const card8 = document.getElementById("card8");
+const img8 = card8.querySelector("img");
+const text8 = card8.querySelector("h3");
+const originalSrc8 = img8.src;
+
+card8.addEventListener("mouseenter", function () {
+    img8.src = "https://asset.kompas.com/crops/iK6IJRCbCf7ypp4H5rFF_7IX9w0=/3x0:700x465/1200x800/data/photo/2021/01/14/5fff83f97a1d3.jpg";
+    text8.style.color = "green";
+    text8.style.transition = "color 0.2s ease-in-out";
+});
+
+card8.addEventListener("mouseleave", function () {
+    img8.src = originalSrc8;
+    text8.style.color = "black";
+});
+
+const card9 = document.getElementById("card9");
+const img9 = card9.querySelector("img");
+const text9 = card9.querySelector("h3");
+const originalSrc9 = img9.src;
+
+card9.addEventListener("mouseenter", function () {
+    img9.src = "https://asset.kompas.com/crops/2ejkrBqZ89aXhXXsPMBYQ8U6r9g=/200x61:1000x594/1200x800/data/photo/2023/07/21/64b9ebbd31630.jpg";
+    text9.style.color = "green";
+    text9.style.transition = "color 0.2s ease-in-out";
+});
+
+card9.addEventListener("mouseleave", function () {
+    img9.src = originalSrc9;
+    text9.style.color = "black";
+});
